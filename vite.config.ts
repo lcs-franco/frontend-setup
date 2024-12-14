be5284b3a43fda5a1664192304f4abe001c9b0e5
Lucas Franco <lcs.franco556@gmail.com>
2024-12-14 15:42:58 -0300

    chore: :wrench: add path alias to projects

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@views': path.resolve(__dirname, 'src', 'views')
+    }
+  }
 })
