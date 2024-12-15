61edca2b1600eb499ce957aa45eb01a41bae05ac
Lucas Franco <lcs.franco556@gmail.com>
2024-12-14 21:50:12 -0300

    chore: :building_construction: change view folder name to ui

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@views': path.resolve(__dirname, 'src', 'views')
+      '@ui': path.resolve(__dirname, 'src', 'ui')
     }
   }
 })
