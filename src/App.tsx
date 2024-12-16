1d4519ceb6e4e0109da49a08e79988313e3c62c0
Lucas Franco <lcs.franco556@gmail.com>
2024-12-15 23:55:50 -0300

    feat: :sparkles: add browser router to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Button from '@ui/components/Button'
+import {BrowserRouter} from 'react-router-dom'
 
 export default function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Button />
-    </>
+    </BrowserRouter>
   )
 }
 
