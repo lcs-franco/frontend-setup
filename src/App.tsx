03bc61784fb60e61daae0055b5e57a9a10d1cc41
Lucas Franco <lcs.franco556@gmail.com>
2024-12-16 21:58:04 -0300

    refactor: :poop: remove test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+
 import Button from '@ui/components/Button'
-import { cn } from '@app/utils/index'
 
 export default function App() {
-  const [state, setState] = useState()
   return (
     <BrowserRouter>
       <Button />
